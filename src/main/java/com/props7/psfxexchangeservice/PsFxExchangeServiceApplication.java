4c23b435a6f8b61aa93ccbe8ec8ffd68f0969638
pawel <7proPS7@gmail.com>
2020-05-01 15:45:59 +0200

    Initial commit

---
+++ src/main/java/com/props7/psfxexchangeservice/PsFxExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.props7.psfxexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsFxExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsFxExchangeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/props7/psfxexchangeservice/PsFxExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.props7.psfxexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsFxExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
